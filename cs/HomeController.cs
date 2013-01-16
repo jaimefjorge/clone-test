0d883819c006b2ccc0d8f71bf6f12c098c6ddbba
Joao Caxaria <caxaria@gmail.com>
2013-01-16 10:46:25 +0000

    added home controller

---
+++ cs/HomeController.cs
@@ -0,0 +1,20 @@
+﻿
+using System.Web.Mvc;
+
+namespace Webpage.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+    }
+}
